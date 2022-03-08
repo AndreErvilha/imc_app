e687662fd47918b7bcf32ad18aa900029b7e02d3
andreervilha <andre.ervilha@cwi.com.br>
2022-03-07 22:47:41 -0300

    code func to calculate and show results of imc

--- imc/ViewController.swift
+++ imc/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var tfHeight: UITextField!
     @IBOutlet weak var lbResult: UILabel!
     @IBOutlet weak var ivResult: UIImageView!
+    @IBOutlet weak var viResult: UIView!
+    
+    var imc: Double = 0
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +27,35 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calculate(_ sender: Any) {
+        if let weight = Double(tfWeight.text!), let height = Double(tfHeight.text!) {
+            imc = weight / (height*height)
+            showResults()
+        }
+    }
+    
+    func showResults() {
+        var result: String = ""
+        var image: String = ""
+        
+        switch imc {
+            case 0..<16:
+                result = "Magreza"
+                image = "abaixo"
+            case 16..<18.5:
+                result = "Abaixo do peso"
+                image = "abaixo"
+            case 25..<30:
+                result = "Peso ideal"
+                image = "ideal"
+            default:
+                result = "Obesidade"
+                image = "obesidade"
+        }
+        
+        lbResult.text = "\(Int(imc)): \(result)"
+        ivResult.image = UIImage(named: image)
+        viResult.isHidden = false
+        view.endEditing(true)
     }
     
 }
